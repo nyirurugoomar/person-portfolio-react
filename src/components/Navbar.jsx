6b7586407edb2e32b6026cdeca9f8d1f9bc0bc36
nyirurugo omar <nyirurugoomar@gmail.com>
2022-06-06 22:08:11 +0200

    nav responsive

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React,{useRef} from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import '../components/styles/main.css'
+
+function Navbar() {
+    const navRef = useRef();
+     
+    const showNavbar = ()=>{
+        navRef.current.classList.toggle('responsive_nav');
+
+    }
+
+  return (
+    <header>
+        <h3>OMAR</h3>
+        <nav ref={ navRef }>
+            <a href='#'>Home</a>
+            <a href='#'>My work</a>
+            <a href='#'>Blog</a>
+            <a href='#'>About Me</a>
+            <button className='nav-btn nav-close-btn'  onClick={showNavbar}>
+               <FaTimes />
+            </button>
+        </nav>
+        <button className='nav-btn'  onClick={showNavbar}>
+           <FaBars />
+        </button>
+    </header>
+  )
+}
+
+export default Navbar
