01ce726e654003ce77ebc77604adbe1fc2d5acf9
nyirurugo omar <nyirurugoomar@gmail.com>
2022-06-17 08:58:37 +0200

    project section updates

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../components/styles/project.css';
+import projectImage from '../assets/projectImage.png'
 
 
 function Project() {
@@ -7,40 +8,81 @@ function Project() {
     <section id="contact">
         <div class="contact container">
             <div>
-                <h1 class="section-title">contact <span>INFO</span></h1>
+                <h1 class="section-title">Projects</h1>
+                
             </div>
             <div class="contact-items">
 
+                
+
                 <div class="contact-item">
-                    <div class="icon"><img src="https://img.icons8.com/ultraviolet/40/000000/phone.png" /></div>
+                    <div class="icon"></div>
                     <div class="contact-info">
-                        <h1>PHONE</h1>
-                        <h2>+250-784-418-127</h2>
+                    <img src={projectImage}/>
+                    <p>PHONEPHONEPHONEPHOHONEPHONE</p>
+                        
                     </div>
                 </div>
 
                 <div class="contact-item">
-                    <div class="icon"><img src="https://img.icons8.com/office/64/000000/email.png" /></div>
+                    <div class="icon"></div>
                     <div class="contact-info">
-                        <h1>EMAIL</h1>
-                        <h2>nyirurugoomar@gmail.com</h2>
+                     <img src={projectImage}/>
+                        <p>PHONEPHONEPHONEPHOHONEPHONE</p>
+                        
+                    </div>
+                <div/>    
+                </div><div class="contact-item">
+                    <div class="icon"></div>
+                    <div class="contact-info">
+                     <img src={projectImage}/>
+                        <p>PHONEPHONEPHONEPHOHONEPHONE</p>
+                        
                     </div>
                 </div>
-
-                <div class="contact-item">
-                    <div class="icon"><img src="https://img.icons8.com/ultraviolet/40/000000/address.png" /></div>
-                    <div class="contact-info">
-                        <h1>ADDRESS</h1>
-                        <h2>KG 238st </h2>
-                    </div>
-                </div>
-
-
 
             </div>
         </div>
+{/* ----------------------------------------------------------------- */}
+     <div className="project2">
+
+        <div class="contact container">
+            
+            <div class="contact-items">
+
+                <div class="contact-item">
+                    <div class="icon"></div>
+                    <div class="contact-info">
+                    <img src={projectImage}/>
+                    <p>PHONEPHONEPHONEPHOHONEPHONE</p>
+                        
+                    </div>
+                </div>
+
+                <div class="contact-item">
+                    <div class="icon"></div>
+                    <div class="contact-info">
+                     <img src={projectImage}/>
+                        <p>PHONEPHONEPHONEPHOHONEPHONE</p>
+                        
+                    </div>
+                <div/>    
+                </div><div class="contact-item">
+                    <div class="icon"></div>
+                    <div class="contact-info">
+                     <img src={projectImage}/>
+                        <p>PHONEPHONEPHONEPHOHONEPHONE</p>
+                        
+                    </div>
+                </div>
+
+            </div>
+        </div>
+      </div>
 
     </section>
+
+    
   )
 }
 
