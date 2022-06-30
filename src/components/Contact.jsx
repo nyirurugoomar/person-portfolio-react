aec0ba065997b3ee82a70820ce7f3ca0ee14f089
nyirurugo omar <nyirurugoomar@gmail.com>
2022-06-30 18:10:30 +0200

    contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import '../components/styles/contact.css'
 
+
 function Contact() {
   return (
-    <h1>Contact section</h1>
+    <div>
+        <div className="contact-container">
+
+            <div className='contact-title'>
+                <h1>Contact</h1>
+                <p>Get in touch</p>
+            </div>
+
+            <div className='contactSection__wrapper'>
+                <div className='left'>
+                    <div className='icon'></div>
+                </div>
+                <div className='right'>
+                    contactForm
+                </div>
+
+            </div>
+
+        </div>
+    </div>
   )
 }
 
