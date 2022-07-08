e8841787a513dddef5f24d167d520f04383a2f47
nyirurugo omar <nyirurugoomar@gmail.com>
2022-07-08 10:59:15 +0200

    phone responsive done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,17 +19,17 @@ function Contact() {
                   <br></br>
                   <a href='mailto:nyirurugoomar@gmail.com'>nyirurugoomar@gmail.com</a>
                   <br></br>
-                  {/* <h1>nyirurugoomar@gmail.com</h1> */}
+                  
                   <label className='i-text'>Phone number</label>
-                  <h1>+(250)784 418 127</h1>
+                  <h1 className='contact-text'>+(250)784 418 127</h1>
                   
                   <label className='i-text'>Address</label>
                   
-                  <h1>Kigali,KG 238st</h1>
+                  <h1 className='contact-text'>Kigali,KG 238st</h1>
                     
                 </div>
                 <div className='right'>
-                    <img src={contactImage} alt='contact image'/>
+                    <img src={contactImage} alt='contact img'/>
                 </div>
 
             </div>
